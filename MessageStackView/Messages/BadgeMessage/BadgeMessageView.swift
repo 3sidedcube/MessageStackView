922a5801e07d08710648bc86d014508208bdf995
Simon Mitchell <simon@3sidedcube.com>
2020-07-17 10:23:35 +0100

    Updates `BadgeMessageView` to have continuous corner curve on iOS 13

--- MessageStackView/Messages/BadgeMessage/BadgeMessageView.swift
+++ MessageStackView/Messages/BadgeMessage/BadgeMessageView.swift
@@ -138,6 +138,11 @@ open class BadgeMessageView: UIView {
         containerView.layer.cornerRadius = layer.cornerRadius
         shadow = BadgeMessageView.shadowComponents
         
+        if #available(iOS 13.0, *) {
+            layer.cornerCurve = .continuous
+            containerView.layer.cornerRadius = layer.cornerRadius
+        }
+        
         // layer
         updateLayer()
         
